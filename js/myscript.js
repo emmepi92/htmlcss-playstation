1a6846f060975757d0cad92ef7153da06fdad027
emmepi92 <mariapiacoccioli@live.it>
2021-05-26 14:25:58 +0200

    js with alert, getElementById & console.log

---
+++ js/myscript.js
@@ -0,0 +1,3 @@
+console.log("Quindi? X-box o Play?");
+document.getElementById("ps5").innerHTML=("Ecco la Nuova PlayStation 5");
+alert("Vedi che la X-box attualmente ha più giochi disponibili (Consiglio spassionato eh)");
